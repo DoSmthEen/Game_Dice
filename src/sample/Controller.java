a137ffe1b0988baa7ea478ba329af6fe5095acad
DanillVasylchenko <danillvasychenko@gmail.com>
2021-04-08 14:09:37 +0300

    Useless libraries and objects deleted

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,8 +5,6 @@ import java.awt.Desktop;
 import java.util.Random;
 import java.util.Date;
 
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -34,76 +32,70 @@ public class Controller{
     Date date;
 
     @FXML
-    private ResourceBundle resources;
+    private Button NewGame_ResetPoints;
 
     @FXML
-    private URL location;
+    private Label CurrentPointsNum;
 
     @FXML
-    private Button Button_NewGame_ResetPoints;
+    private Label GameResults;
 
     @FXML
-    private Label Current_Points_Num_Text;
+    private TextField InputBet;
 
     @FXML
-    private Label Game_Results;
+    private TextField InputPrediction;
 
     @FXML
-    private TextField Field_Input_Bet;
+    private Button ShowGameLog;
 
     @FXML
-    private TextField Field_Input_Prediction;
-
-    @FXML
-    private Button Button_ShowGameLog;
-
-    @FXML
-    private Button Button_Play;
+    private Button Play;
 
     @FXML
     private Label Errors;
 
     @FXML
-    private Button Button_Save_In_File;
+    private Button SaveInFile;
 
     @FXML
     void initialize(){
-        Button_Save_In_File.setDisable(true);
-        Button_Save_In_File.setVisible(false);
+        SaveInFile.setDisable(true);
+        SaveInFile.setVisible(false);
 
-        Button_ShowGameLog.setOnAction(event -> {
+        ShowGameLog.setOnAction(event -> {
             if (!file.exists()){
                 try {
                     file.createNewFile();
-                } catch (IOException e) {
+                } catch (IOException ignored) {
                 }
             }
             try {
                 desktop.open(file);
-            } catch (IOException e) {
+            } catch (IOException ignored) {
             }
         });
 
-        Button_NewGame_ResetPoints.setOnAction(event -> {
-            Current_Points_Num_Text.setText("100");
-            Button_Save_In_File.setDisable(true);
-            Button_Save_In_File.setVisible(false);
-            Game_Results.setText("");
+        NewGame_ResetPoints.setOnAction(event -> {
+            CurrentPointsNum.setText("100");
+            SaveInFile.setDisable(true);
+            SaveInFile.setVisible(false);
+            GameResults.setText("");
             Errors.setText("");
         });
 
-        Button_Play.setOnAction(event ->{
-            current_points_num = Integer.parseUnsignedInt(Current_Points_Num_Text.getText());
+        Play.setOnAction(event ->{
+            current_points_num = Integer.parseUnsignedInt(CurrentPointsNum.getText());
             try {
-                bet = Integer.parseUnsignedInt(Field_Input_Bet.getText());
-                prediction = Integer.parseUnsignedInt(Field_Input_Prediction.getText());
+                bet = Integer.parseUnsignedInt(InputBet.getText());
+                prediction = Integer.parseUnsignedInt(InputPrediction.getText());
                 if (bet > current_points_num){
                     errors_list = errors_list + "-You don't have enough points!\n";
-                    Game_Results.setText("");
+                    GameResults.setText("");
                 }
                 if (prediction > 12 || prediction < 2){
                     errors_list = errors_list + "-Prediction must be in range\n of 2 to 12(inclusively)!";
-                    Game_Results.setText("");
+                    GameResults.setText("");
                 }
                 else if (bet <= current_points_num) {
                     first_dice = random.nextInt(7-1) + 1;
@@ -112,36 +104,36 @@ public class Controller{
 
                     if(prediction == dices_sum){
                         results = results + "YOU WIN!" + "\nPoints won: " + String.valueOf(4 * bet) + "\n\n";
-                        Current_Points_Num_Text.setText(String.valueOf(current_points_num + 4 * bet));
+                        CurrentPointsNum.setText(String.valueOf(current_points_num + 4 * bet));
                     }
                     else if((prediction <= 7 && dices_sum <= 7) || (prediction > 7 && dices_sum > 7)){
                         results = results + "YOU WIN!" + "\nPoints won: " + String.valueOf(bet) + "\n\n";
-                        Current_Points_Num_Text.setText(String.valueOf(current_points_num + bet));
+                        CurrentPointsNum.setText(String.valueOf(current_points_num + bet));
                     }
                     else {
                         results = results + "YOU LOSE!" + "\nPoints lost: " + String.valueOf(bet) + "\n\n";
-                        Current_Points_Num_Text.setText(String.valueOf(current_points_num - bet));
+                        CurrentPointsNum.setText(String.valueOf(current_points_num - bet));
                     }
                     results = results + "First dice: " + String.valueOf(first_dice) + "\nSecond dice: "
                     + String.valueOf(second_dice) + "\nSum of points on dices: " + String.valueOf(dices_sum)
                             + "\nYour bet: " + String.valueOf(bet) + "\nYour prediction: " + String.valueOf(prediction);
-                    Game_Results.setText(results);
+                    GameResults.setText(results);
                     results = "";
 
-                    Button_Save_In_File.setDisable(false);
-                    Button_Save_In_File.setVisible(true);
+                    SaveInFile.setDisable(false);
+                    SaveInFile.setVisible(true);
                 }
             }
             catch (NumberFormatException nfe){
                 errors_list = errors_list + "-Gaps must be filled\nwith Non-negative numbers!\n";
-                Game_Results.setText("");
+                GameResults.setText("");
             }
             Errors.setText(errors_list);
             errors_list = "";
         });
 
-        Button_Save_In_File.setOnAction(event -> {
-            if(Game_Results.getText().equals("")){
+        SaveInFile.setOnAction(event -> {
+            if(GameResults.getText().equals("")){
                 Errors.setText("Play first!");
             }
             else {
@@ -150,13 +142,13 @@ public class Controller{
                 {
                     try {
                         write_result = new BufferedWriter(new FileWriter(file, true));
-                        write_result.write("-" + date.toString() + "-\n" + Game_Results.getText() +
+                        write_result.write("-" + date.toString() + "-\n" + GameResults.getText() +
                                 "\n********************\n");
                         write_result.close();
 
-                        Button_Save_In_File.setDisable(true);
-                        Button_Save_In_File.setVisible(false);
-                    } catch (IOException e) {
+                        SaveInFile.setDisable(true);
+                        SaveInFile.setVisible(false);
+                    } catch (IOException ignored) {
                     }
                 }
             }
