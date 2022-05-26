f56c81854714d84e4ecdec83fd96ae2d75857665
ECuUMCWf <ecuumcwf@email.com>
2022-05-26 22:22:02 +0300

    kek

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,10 +4,13 @@ package sample;
         import javafx.scene.Parent;
         import javafx.scene.Scene;
         import javafx.stage.Stage;
+
+        import java.util.Objects;
+
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
         primaryStage.setTitle("Dice Game");
         primaryStage.setScene(new Scene(root, 700, 500));
         primaryStage.setResizable(false);
